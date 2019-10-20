3cd519b957bd22a850619f237a1baca52a88b06d
Simon Colmer <simon@colmer.me>
2019-10-20 04:46:20 +0100

    Add profile page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-// src/App.js
-
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return (
-      <div>Loading...</div>
-    );
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
+import './index.css';
 
 // A function that routes the user to the right place
 // after login
