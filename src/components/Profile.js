03fc8b1dc2ecdb5780ad8ee8c06b52b06c82bf78
Simon Colmer <simon@colmer.me>
2019-10-24 22:49:50 +0100

    Add support to find AccessToken

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,25 @@
-// src/components/Profile.js
-
 import React, { Fragment } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 
 const Profile = () => {
-  const { loading, user } = useAuth0();
+  const { loading, user, getTokenSilently } = useAuth0();
 
   if (loading || !user) {
     return (
       <div>Loading...</div>
     );
   }
+
+
+  const callApi = async () => {
+    try {
+      const token = await getTokenSilently();
+
+      console.log(token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Fragment>
@@ -19,6 +28,8 @@ const Profile = () => {
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
+
+      <button onClick={callApi}>Get Access Token</button>
     </Fragment>
   );
 };
